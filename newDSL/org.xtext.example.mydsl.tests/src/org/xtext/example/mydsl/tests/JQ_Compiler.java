ba6eb7190504918e7908621025890f0906b2d617
Tom.Roy <tom.roy@insa-rennes.fr>
2023-01-06 16:45:14 +0100

    Modification sur les compilers

---
+++ newDSL/org.xtext.example.mydsl.tests/src/org/xtext/example/mydsl/tests/JQ_Compiler.java
@@ -0,0 +1,123 @@
+package org.xtext.example.mydsl.tests;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.eclipse.emf.common.util.EList;
+import org.xtext.example.mydsl.myDsl.Fun;
+import org.xtext.example.mydsl.myDsl.Model;
+
+import org.xtext.example.mydsl.myDsl.ReadJson;
+import org.xtext.example.mydsl.myDsl.Remove;
+import org.xtext.example.mydsl.myDsl.WriteCSV;
+import org.xtext.example.mydsl.myDsl.WriteJson;
+import org.xtext.example.mydsl.myDsl.JSON;
+import org.xtext.example.mydsl.myDsl.Add;
+import org.xtext.example.mydsl.myDsl.Sort;
+
+import com.google.common.io.Files;
+
+public class JQ_Compiler {
+	
+	private Model _model;
+	
+	JQ_Compiler(Model model){
+		_model = model;
+	}
+	public void compileAndRun() throws IOException {
+		
+		//Code generation
+		EList<Fun> sentences = _model.getSentences();
+
+	
+		String JQCode="";
+		JQCode+=" #!/bin/bash \n";
+		
+		for(Fun s : sentences) {
+			
+			if( s instanceof Show) {
+				Show so = (Show) s;
+				
+				String Name = so.getName();
+				
+				JQCode += " jq '.' "+Name+"\n"; 
+				
+			}
+			
+			if (s instanceof WriteJson) {
+				WriteJson w = (WriteJson) s;
+				
+				String RegisterFileName= w.getPath();
+				JSON data= w.getFile();
+				EList<String> atts= data.getAttribute();
+				EList<String> vals= data.getValue();
+				int lenght=atts.size();
+				
+				
+				JQCode+="jq -n";
+				for(int i=0;i<lenght;i++) {
+					JQCode+="--arg "+atts.get(i)+" \""+vals.get(i)+" "; 
+					
+				}
+				JQCode+= ">"+RegisterFileName+ "\n";
+			}
+			
+			if(s instanceof WriteCSV) {
+				WriteCSV w= (WriteCSV) s;
+				
+				String RegisterFileName= w.getPath();
+				JSON data= w.getFile();
+				EList<String> atts= data.getAttribute();
+				EList<String> vals= data.getValue();
+				int lenght=atts.size();
+				
+				String JSONSTR= "{ ";
+				for(int i=0;i<lenght;i++) {
+					JSONSTR+=atts.get(i)+" : "+vals.get(i)+" , ";
+					
+				}
+				JSONSTR+=" } ";
+				
+				JQCode+= "echo '"+JSONSTR+"' |js -r '. | @csv' >"+RegisterFileName+"\n";
+			}
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+		String JQ_OUTPUT = "foo.sh";
+
+		// or shorter
+		Files.write(JQCode.getBytes(), new File(JQ_OUTPUT));
+		
+		
+		Process p = Runtime.getRuntime().exec("./ "+ JQ_OUTPUT);
+		
+		// output
+		BufferedReader stdInput = new BufferedReader(new
+				InputStreamReader(p.getInputStream()));
+		
+		// error
+		BufferedReader stdError = new BufferedReader(new
+				InputStreamReader(p.getErrorStream()));
+		
+		
+	    String o;
+		while ((o = stdInput.readLine()) != null) {
+	        System.out.println(o);
+	    }
+	    
+		String err; 
+		while ((err = stdError.readLine()) != null) {
+	        System.out.println(err);
+	    }
+		
+	}
+
+}
--- newDSL/org.xtext.example.mydsl.tests/src/org/xtext/example/mydsl/tests/Python_Compiler.java
+++ newDSL/org.xtext.example.mydsl.tests/src/org/xtext/example/mydsl/tests/Python_Compiler.java
@@ -5,14 +5,19 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.emf.common.util.EList;
 import org.xtext.example.mydsl.myDsl.Fun;
 import org.xtext.example.mydsl.myDsl.Model;
 import org.xtext.example.mydsl.myDsl.ReadJson;
+import org.xtext.example.mydsl.myDsl.Remove;
 import org.xtext.example.mydsl.myDsl.WriteCSV;
 import org.xtext.example.mydsl.myDsl.WriteJson;
 import org.xtext.example.mydsl.myDsl.JSON;
+import org.xtext.example.mydsl.myDsl.Add;
+import org.xtext.example.mydsl.myDsl.Sort;
 
 import com.google.common.io.Files;
 
@@ -30,18 +35,32 @@ public class Python_Compiler {
 		String pythonCode="";
 		pythonCode+="import pandas as pd\n"
 				+ "import json \n";
+		
+		// Vrai si un JSON a été lu
 		boolean HaveReadJSON=false;
+		
+		// List du nom des variables
+		List<String> VarName= new ArrayList<String>();
+		
+		// Indique qu'il faut utiliser la derniere variable, vrai par default
+		boolean UseLast=true;
+		
+		
 		for(Fun s : sentences) {
 			
+			
+			// Lecture d'un JSON
 			if (s instanceof ReadJson) {
 				
 				ReadJson r = (ReadJson) s;
 				// r.get
 				String fileName = r.getPath();
-				pythonCode+="df = pd.read_json(\""+fileName+"\")\n";
+				VarName.add("df"+VarName.size());
+				pythonCode+=VarName.get(VarName.size()-1)+" = pd.read_json(\"" + fileName+ "\" )\n";
 				HaveReadJSON=true;
 			}
 			
+			// Ecriture d'un JSON en .json
 			if(s instanceof WriteJson) {
 				WriteJson r =(WriteJson) s;
 				
@@ -51,14 +70,16 @@ public class Python_Compiler {
 				EList<String> atts= data.getAttribute();
 				EList<String> vals= data.getValue();
 				int lenght=atts.size();
-				pythonCode+="json.dump({";
+				pythonCode+="json.dump( {";
 				for(int i=0;i<lenght;i++) {
-					pythonCode+=atts.get(i)+":"+vals.get(i)+",";
+					pythonCode+=atts.get(i)+" : "+vals.get(i)+" , ";
 					
 				}
-				pythonCode+="},"+RegisterFileName+")\n";
+				pythonCode+=" }, "+RegisterFileName+" )\n";
 				
 			}
+			
+			//Ecriture d'un JSON en .csv
 			if(s instanceof WriteCSV) {
 				WriteCSV w=(WriteCSV) s;
 				
@@ -70,15 +91,118 @@ public class Python_Compiler {
 				
 				//pythonCode+="df2 = pd.read_json(jsonStr, orient ='index')";
 				
-				String JSONSTR= "{";
+				String JSONSTR= "{ ";
 				for(int i=0;i<lenght;i++) {
-					JSONSTR+=atts.get(i)+":"+vals.get(i)+",";
+					JSONSTR+=atts.get(i)+" : "+vals.get(i)+" , ";
 					
 				}
-				JSONSTR+="}";
+				JSONSTR+=" } ";
 				
-				pythonCode+="df = pd.read_json("+JSONSTR+", orient ='index')";
+				pythonCode+="df = pd.read_json("+JSONSTR+", orient ='index') \n";
+				pythonCode+="df.to_csv("+RegisterFileName+",index=False) \n";
 			}
+			
+			
+			// Remove un element d'un JSON via son id
+			
+			if(s instanceof Remove) {
+				Remove r = (Remove) s;
+				int id= r.getId();
+				
+				JSON data=r.getFile();
+				
+				EList<String> atts = data.getAttribute();
+				EList<String> vals = data.getValue();
+				int lenght=atts.size();
+				String JSONSTR = "{";
+				for(int i=0;i<lenght;i++) {
+					if(i!=id) {
+						JSONSTR+=atts.get(i)+":"+vals.get(i)+",";
+					}	
+				}
+				JSONSTR+="}";
+				if(UseLast) {
+					pythonCode+=VarName.get(VarName.size()-1)+" = pd.read_json("+JSONSTR+", orient ='index') \\n";
+				}
+				//pythonCode+="df = pd.read_json("+JSONSTR+", orient ='index') \n";
+				
+				
+			}
+			
+			// Ajoute un élément au dernier JSON lu.
+			if( s instanceof Add) {
+				Add a = (Add) s;
+				//JSON auquel on ajoute
+				//JSON data = a.getFile();
+				//EList<String> atts = data.getAttribute();
+				//EList<String> vals = data.getValue();
+				//OU
+				// Si enregistrer dans les variables
+				String Name= VarName.get(VarName.size()-1);
+				
+				//JSON a ajouter
+				JSON data = a.getData();
+				if(data!=null) {
+					EList<String> atts = data.getAttribute();
+					EList<String> vals = data.getValue();
+					int lenght=atts.size();
+					String JSONSTR = "{";
+					for(int i=0;i<lenght;i++) {
+						if(i!=id) {
+							JSONSTR+=atts.get(i)+":"+vals.get(i)+",";
+						}	
+					}
+					JSONSTR+="}";
+					
+					pythonCode+="tmpDF = pd.read_json("+JSONSTR+", orient ='index') \\n";
+					
+				}
+				
+				pythonCode += Name+".append(tmpDF) \\n";
+				
+			}
+			
+			//Montre le JSON choisi par son nom
+			if (s instanceof Show) {
+				Show sh= (Show) s;
+				String name = sh.getName();
+				pythonCode+= "display("+name+".to_string()) \\n";
+			}
+			
+			// Obtient la valeur par ID du dernier JSON ouvert
+			if (s instanceof Get) {
+				Get g= (Get) g;
+				
+				//JSON auquel on ajoute
+				//JSON data = a.getFile();
+				//EList<String> atts = data.getAttribute();
+				//EList<String> vals = data.getValue();
+				//OU
+				// Si enregistrer dans les variables
+				String Name= VarName.get(VarName.size()-1);
+				int id= g.getId();
+				pythonCode+= Name+ "["+id + "]";
+				
+				
+			}
+			
+			// Trie le dernier JSON ouvert
+			if (s instanceof Sort) {
+				Sort so = (Sort) s;
+				
+				
+				//JSON auquel on ajoute
+				//JSON data = a.getFile();
+				//EList<String> atts = data.getAttribute();
+				//EList<String> vals = data.getValue();
+				//OU
+				// Si enregistrer dans les variables
+				String Name= VarName.get(VarName.size()-1);
+				String attname=so.getAttribute();
+				pythonCode+= Name+".sort_values(by = "+attname+")";
+				
+			}
+			
 		}
 		
 		//pythonCode += "print(df)";
